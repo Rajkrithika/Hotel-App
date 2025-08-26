5ff995173f466340fed66a7e2c6c4426bc6eda1f
Rajkrithika <mail2krithika1@gmail.com>
2025-08-25 22:12:32 -0400

    service created

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
-  styleUrl: './reservation-form.component.css'
+  styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
+ reservationForm : FormGroup = new FormGroup({});
+ constructor(private formBuilder:FormBuilder){
+
+ }
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      guestName: [''],
+      guestEmail: [''],
+      checkInDate: ['',Validators.required],
+      checkOutDate: ['',Validators.required],
+      roomNumber: ['',Validators.required]
+    });
+    
+  }
+ 
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      console.log("valid");
+    }
+  }
+
 
 }
